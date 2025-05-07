72a71bd54e1a3dfe75681f526be3d3ad2a833102
alamvai726 <jubaa4091@gmail.com>
2025-05-07 09:55:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ logger("Opened server site...", "[ Starting ]");
 function startBot(message) {
     (message) ? logger(message, "[ Starting ]") : "";
 
-    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "Jubayer.js"], {
+    const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "mirai.js"], {
         cwd: __dirname,
         stdio: "inherit",
         shell: true
